06f5c629fed122af07121f2c848d182bbe0b001d
sumanthm264 <157963007+sumanthm264@users.noreply.github.com>
2024-08-11 03:42:08 +0000

    Pending changes exported from your codespace

---
+++ scripts/mint.js
@@ -0,0 +1,37 @@
+const hre = require("hardhat");
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpcLink = hre.network.config.url;
+  const [encryptedData] = await encryptDataField(rpcLink, data);
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  const contractAddress = "0x710d5055D627EE284E1F7d2aa5400D78D8Dd7371";
+  const [signer] = await hre.ethers.getSigners();
+
+  const contractFactory = await hre.ethers.getContractFactory("TestToken");
+  const contract = contractFactory.attach(contractAddress);
+
+  const functionName = "mint100tokens";
+  const mint100TokensTx = await sendShieldedTransaction(
+    signer,
+    contractAddress,
+    contract.interface.encodeFunctionData(functionName),
+    0
+  );
+
+  await mint100TokensTx.wait();
+
+  console.log("Transaction Receipt: ", mint100TokensTx.hash);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
